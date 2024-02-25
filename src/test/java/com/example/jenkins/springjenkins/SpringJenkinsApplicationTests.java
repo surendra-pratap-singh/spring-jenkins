a564a43c8b20fb9f826b1bd88642f870d69f00d7
Surendra Pratap Singh Chauhan <surendra.chauhan@28stone.com>
2024-02-25 19:32:32 +0200

    first commit

---
+++ src/test/java/com/example/jenkins/springjenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example.jenkins.springjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+    public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
+    @Test
+    public void contextLoads() {
+        logger.info("test case running...");
+        assertEquals(true, true);
+    }
+
+}
